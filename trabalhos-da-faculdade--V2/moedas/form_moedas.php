668d949dddf5e27df2e3814b20a14ce4b0cc0df9
rodrigo-americo <rodrigo_7_4_7@hotmail.com>
2021-06-03 22:50:22 -0300

    v.71 carrinho fuincionado

--- trabalhos-da-faculdade--V2/moedas/form_moedas.php
+++ trabalhos-da-faculdade--V2/moedas/form_moedas.php
@@ -1,13 +1,40 @@
 <?php
 session_start();
+include ('scripts-antigos(JSON)\funcao_carrinho.php');
+include ('scripts-antigos(JSON)\carrinho.php');
+include ('scripts-antigos(JSON)\lejson.php');
 
+// chama a funcao de pegar a data atual
+$data = geradata();
+// n permite que seja gerado mais de 1 carrinho e estancia varios valores (mover para aquivo carinho)
+if (!isset($_SESSION['carrinho'])) {
+    cria_carinho();
+}
+// calcula desconto
+if (isset($_POST['desconto'])) {
+    $_SESSION['desconto'] = $_POST['desconto'] / 100;
+}
+// reseta o carrinho
+if (isset($_POST['resetar'])) {
+    resetar();
+}
+// base para adicionar 
+if ( isset($_POST['produtos'])) {
+    adicionar($_POST['produtos']);
+}
+
+$_SESSION['valor_com_desconto'] = $_SESSION['valortotal'] - ($_SESSION['valortotal'] * $_SESSION['desconto']);
+
+if (isset($_POST['enviar'])) {
+    criatxt($data);
+}
 ?>
 <HTML>
 
 <HEAD>
-   <!-- CSS only -->
-   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-   <TITLE>Exemplo de Formulario - leitura dos dados</TITLE>
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <TITLE>Exemplo de Formulario - leitura dos dados</TITLE>
 </HEAD>
 
 <BODY>
@@ -17,30 +44,27 @@ session_start();
             <div class="container">
                 <FORM ACTION="form_moedas.php" METHOD="post">
                     <div class="row">
-                        <div class="col-sm-12 col-md-6">
-                            <div class="mb-3">
-                                <label for="Nome" class="form-label">Nome</label>
-                                <input name="nome" type="text" class="form-control" id="Nome">
-                            </div>
-                        </div>
                         <div class="col-sm-12 col-md-6 ">
                             <div class="mb-3">
                                 <label for="Produtos" class="form-label">Produtos</label>
+                                <?php
+                                cria_lista_de_produtos();
+                                ?>
                             </div>
                         </div>
                         <div class="col-sm-12 col-md-6 ">
                             <div class="mb-3">
                                 <label for="cupom" class="form-label">Valor do cupom</label>
                                 <select name='desconto' class="form-select" aria-label="Default select example" id="cupom">
-                                <option value="3" selected>3%</option>
-                                <option value="5">5%</option>
-                                <option value="10">10%</option>
+                                    <option value="3" selected>3%</option>
+                                    <option value="5">5%</option>
+                                    <option value="10">10%</option>
                                 </select>
                             </div>
                         </div>
                         <div class="col-sm-12 col-md-6 mt-4  ">
                             <div class="d-grid gap-2">
-                                <button type="submit" class="btn btn-outline-success btn-block " name='adicionar' value="ADICIONAR">adicionar</button>
+                                <button type="submit"  class="btn btn-outline-success btn-block  " name='adicionar' value="ADICIONAR">adicionar</button>
                             </div>
                         </div>
                     </div>
@@ -84,4 +108,5 @@ session_start();
     </div>
 
 </BODY>
+
 </HTML>
--- trabalhos-da-faculdade--V2/moedas/scripts-antigos(JSON)/funcao_carrinho.php
+++ trabalhos-da-faculdade--V2/moedas/scripts-antigos(JSON)/funcao_carrinho.php
@@ -1,8 +1,9 @@
 <?php
+include ("../php/conexao-bd/conexao.php");
 // teste se ja tem o mesmo item do carinho 
-function ja_tem($nome_produto){
+function ja_tem($ID){
     foreach ($_SESSION['carrinho'] as $produto) {
-        if ($produto['nome_do_produto'] === $nome_produto) {
+        if ($produto['ID'] == $ID) {
             return true;
         }
     }
@@ -14,23 +15,39 @@ function resetar(){
     $_SESSION['valortotal'] = 0;
 }
 // atribui itens ao carinho
-function adicionar($nome, $nome_produto){
-    if (!ja_tem($nome_produto)) {
-        $produtos = lejason('Produtos.json');
-        foreach($produtos as $produto){
-            $_SESSION['valortotal'] = $_SESSION['valortotal'] + floatval($produto->valor);
-            array_push($_SESSION['carrinho'], array('nome' => $nome, 'nome_do_produto' => $produto->nome_do_produto, 'valor' => $produto->valor));
-        }
-
+function adicionar($ID){
+    if (!ja_tem($ID)) {
+        $produto = buscarProduto($ID);
+        $_SESSION['valortotal'] += floatval($produto['Valor']);
+        array_push($_SESSION['carrinho'], array('ID' => $produto['ID']));
     }
 }
-
+function ligaBD(){
+    $servidor = "localhost";
+    $usuario = "root";
+    $senha = "";
+    $dbname = "php-fatec";
+    $conn = mysqli_connect($servidor, $usuario, $senha, $dbname) or die("Falha conexão");
+    return $conn;
+}
+function lerBd() {
+    $conn=ligaBD();
+    $produtos=mysqli_query($conn,' SELECT * FROM produtos ') ;
+    return $produtos;
+}
+function buscarProduto($ID) {
+    $conn=ligaBD();
+    $produto=mysqli_query($conn,' SELECT * FROM produtos WHERE ID='.$ID.'') ;
+    $produto= mysqli_fetch_array($produto);
+    return $produto;
+}
 function cria_lista_de_produtos(){
-    $produtos =lejason('Produtos.json');
+    $produtos = lerBd();
 $lista_de_produtos = '<SELECT NAME="produtos" class="form-select" required>';
-    foreach ($produtos as $produto) {
-    $outrascoisas = $produto->nome . '-' . $produto->valor;
-    $lista_de_produtos .= '<OPTION VALUE="' . $outrascoisas . '">' . $produto->nome_do_produto . ' R$ ' . $produto->valor . '</OPTION>';
+    echo $lista_de_produtos;
+    foreach ($produtos as $produto){
+        $produto ='<option value='.$produto['ID'].'> '.$produto['Nome'].'valor '.$produto['Valor'].'</option>';
+        echo $produto;
     }
 $lista_de_produtos .= '</SELECT>';
     echo $lista_de_produtos;
